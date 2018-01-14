b7a2f5967fbbf6b8cada825b02b5e788971c5c62
vinsonXA <34568582+vinsonXA@users.noreply.github.com>
2018-01-14 12:16:56 +0800

    Add files via upload

---
+++ LoginController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+namespace MvcApplication1.Controllers
+{
+    public class LoginController : Controller
+    {
+        //  
+        // GET: /Login/  
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [AcceptVerbs(HttpVerbs.Post)]
+        public void Index(string username, string password)
+        {
+            Models.Login_BS l_bs = new MvcApplication1.Models.Login_BS();
+            if (l_bs.login(username, password))
+            {
+                Response.Write("登陆成功,用户名称为：" + username);
+            }
+            else
+            {
+                Response.Write("登陆失败");
+
+            }
+        }
+    }
+}
